e0830462a283a8f6a9bb20d5a2b01d443ef7e14a
Success-akinyemi <successakinyemi365@gmail.com>
2024-12-15 00:11:43 +0100

    get playlist

--- controllers/playList.controllers.js
+++ controllers/playList.controllers.js
@@ -121,30 +121,36 @@ export async function getUserAllPlayList(req, res) {
 }
 
 export async function getUserPlayList(req, res) {
-    const { _id: playListId } = req.params
-    const { _id } = req.user
-    if(!playListId){
-        return res.status(400).json({ success: false, data: 'Provide a Id'})
-    }
-    try {
-        const userPlayList = await PlayListModel.findById({ _id: playListId })   
+    const { _id: playListId } = req.params;
+    const { _id } = req.user;
 
-        if(!userPlayList){
-            return res.status(404).json({ success: false, data: 'Play list does not exist'})
+    if (!playListId) {
+        return res.status(400).json({ success: false, data: 'Provide an ID' });
+    }
+
+    try {
+        const userPlayList = await PlayListModel.findById({ _id: playListId });
+
+        if (!userPlayList) {
+            return res.status(404).json({ success: false, data: 'Playlist does not exist' });
         }
 
-        const tracksArray = userPlayList?.tracksId
+        const tracksArray = userPlayList?.tracksId;
         const songs = await SongModel.find({ trackId: { $in: tracksArray } }).select(`-_id -lyrics -trackUrl`);
 
+        const processedSongs = songs.map(song => ({
+            ...song._doc,
+            likes: song.likes ? song.likes.length : 0 
+        }));
 
         const data = {
             name: userPlayList?.name,
-            tracks: songs
-        }
-        res.status(200).json({ success: true, data: data })
+            tracks: processedSongs
+        };
+
+        res.status(200).json({ success: true, data: data });
     } catch (error) {
-        console.log('UNABLE TO GET USER PLAY LIST', error)
-        res.status(500).josn({ success: false, data: 'Unable to get user play lists' })
+        console.error('UNABLE TO GET USER PLAYLIST', error);
+        res.status(500).json({ success: false, data: 'Unable to get user playlists' });
     }
 }
-
